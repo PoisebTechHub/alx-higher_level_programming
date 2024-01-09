4fe41832c875caf9173277af0c268cc9ca336a26
PoisebTechHub <sadikadams23@gmail.com>
2024-01-09 12:28:26 -0800

    update

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,9 +3,10 @@
 # Write a function that replaces an element in a list
 # at a specific position without modification.
 def new_in_list(my_list, idx, element):
+    node = my_list[:]
     if idx < 0:   # when idx is negative return list
-        return my_list
+        return node
     if idx > len(my_list) - 1:   # when idx is greater than elements in list
-        return my_list    # retun list
-    my_list[idx] = element   # Replacing element in list
-    return my_list
+        return node    # retun node list
+    node[idx] = element   # Replacing element in list node
+    return node
