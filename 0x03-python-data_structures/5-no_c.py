3b55d8accee0db06c1742878efe978c67c550068
PoisebTechHub <sadikadams23@gmail.com>
2024-01-09 22:08:21 -0800

    update

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -5,5 +5,5 @@ def no_c(my_string):  # Prototype given
     if my_string[:]:
         new_string = my_string.translate({ord("c"): None})
         second_string = new_string.translate({ord("C"): None})
-            return second_string
-        return my_string
+        return second_string
+    return my_string
