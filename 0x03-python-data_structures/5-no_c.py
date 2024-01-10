8670f2204180dac8b99884ee1f00fd250b00e128
PoisebTechHub <sadikadams23@gmail.com>
2024-01-09 22:00:46 -0800

    update

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,5 +3,6 @@
 # Write a function that removes all characters c and C from a string
 def no_c(my_string):  # Prototype given
     if my_string[:]:
-        copy = [x for x in my_string if x != 'c' and x != 'C']
-    return ("".join(copy))
+        new_string = my_string.translate({ord("c"): None})
+        second_string = new_string.translate({ord("C"): None})
+            return second_string
