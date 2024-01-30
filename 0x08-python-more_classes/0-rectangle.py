e3ef15540079cc0de5b245c15f6bf511fb9a7838
PoisebTechHub <sadikadams23@gmail.com>
2024-01-29 17:58:34 -0800

    updated

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+'''
+1. Write an empty class Rectangle that defines a rectangle:
+2. You are not allowed to import any module
+Below is the solution
+'''
+
+
+""" This module contains class Rectangle """
+
+
+class Rectangle:
+    """ Rectangle is empty """
+    pass
