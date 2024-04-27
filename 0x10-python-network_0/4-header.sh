3a5cf7f29aa0971b5f69e80c60c1084866b1bdeb
“PoisebTechHub” <sadikadams23@gmail.com>
2024-04-27 11:59:28 +0000

    python network 0

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# Sends a GET request to the URL, and displays the body of response
-curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
+# takes in a URL as an argument, sends a GET request to the URL,
+# and displays the body of the response.
+curl -s -H "X-School-User-Id:98" "$1"
